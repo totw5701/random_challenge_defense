752f9d469479897696e7b8275cc576ea2e14be18
totw5701 <inw2129@gmail.com>
2025-04-01 11:40:19 +0900

    프로젝트시작

---
+++ src/main/java/com/rcd/rcdapi/RcdApiApplication.java
@@ -0,0 +1,13 @@
+package com.rcd.rcdapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcdApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RcdApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/rcd/rcdapi/RcdApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rcd.rcdapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RcdApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
